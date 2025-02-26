25ae4183dd2d3a1a1d9454d70eebf8ebda981f4d
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2025-02-26 22:04:15 +0530

    Subtract Added and Count Extracted

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,12 @@ function App() {
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                    Add
                 </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
+                <button onClick={() => setCount((count) => count - 1)}>
+                    Subtract
+                </button>
+                <p>count is {count}</p>
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
